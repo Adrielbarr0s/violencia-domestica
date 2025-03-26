4abd21c4e6e2410717c3050348e554ce9656a9a1
Adrielbarr0s <adrielberrios.10@gmail.com>
2025-03-26 16:51:44 -0300

    Criação de componente.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './component/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [NewComponent],
+  template:
+    `
+    <!-- <router-outlet></router-outlet> -->
+    <h1>Ajuda no combate à violência doméstica</h1>
+    <app-new-component />
+    `,
+  styles: [`
+    *{
+      background-color:rgba(152, 168, 247, 0.3);
+    }
+    h1 {
+      color: #369;
+      font-family: Helvetica;
+      font-size: 250%;
+      text-align: center;
+    }
+  `]
 })
 export class AppComponent {
   title = 'violencia-domestica';
