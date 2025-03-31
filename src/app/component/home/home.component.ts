8d89c8415cc5895f81cb8fe3e2f7c8852efe148d
Adrielbarr0s <adrielberrios.10@gmail.com>
2025-03-31 12:39:58 -0300

    Criação de carrossel

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
