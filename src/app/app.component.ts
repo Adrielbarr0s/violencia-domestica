308f84a5fd01e530231d68cfc4d963c5a3679f4d
Adrielbarr0s <adrielberrios.10@gmail.com>
2025-04-02 19:22:30 -0300

    Implementação de menu hamburguer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'violencia-domestica';
+
+  //lógica para o menu hamburguer
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  } 
 }
