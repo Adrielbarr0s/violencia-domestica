ec07fe5cc8acc19697585edef9d175ab986c06c7
Adrielbarr0s <adrielberrios.10@gmail.com>
2025-04-03 14:25:21 -0300

    Implementação de cards.

--- src/app/component/servicos/servicos.component.ts
+++ src/app/component/servicos/servicos.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'; // Importado MatCardModule
+import { MatButtonModule } from '@angular/material/button'; // Importado MatButtonModule
 
 @Component({
   selector: 'app-servicos',
-  imports: [],
+standalone: true,
+  imports: [MatCardModule, MatButtonModule], // Adicionados MatCardModule e MatButtonModule
   templateUrl: './servicos.component.html',
-  styleUrl: './servicos.component.scss'
+  styleUrls: ['./servicos.component.scss']
 })
 export class ServicosComponent {
 
