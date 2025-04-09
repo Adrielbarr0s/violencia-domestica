160efd8e8e693dc9acd7fe31e5004b3106ff78ca
Adrielbarr0s <adrielberrios.10@gmail.com>
2025-04-09 17:08:55 -0300

    Implementação de cards.

--- src/app/component/contato/contato.component.ts
+++ src/app/component/contato/contato.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'; // Importado MatCardModule
+import { MatButtonModule } from '@angular/material/button'; // Importado MatButtonModule
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
--- src/app/component/sobre/sobre.component.ts
+++ src/app/component/sobre/sobre.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-sobre',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './sobre.component.html',
   styleUrl: './sobre.component.scss'
 })
