ba11943158d48336ecaa87f4af41cde436527ddb
Adrielbarr0s <adrielberrios.10@gmail.com>
2025-04-10 12:36:09 -0300

    Alteração na pagina sobre, serviços e nos icones dos contatos

--- src/app/component/contato/contato.component.ts
+++ src/app/component/contato/contato.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'; // Importado MatCardModule
 import { MatButtonModule } from '@angular/material/button'; // Importado MatButtonModule
+import { MatIconModule } from '@angular/material/icon'; // Importado MatIconModule
 
 @Component({
   selector: 'app-contato',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
