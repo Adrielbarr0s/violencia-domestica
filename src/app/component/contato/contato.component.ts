5aa1bb3e0daec4eeb0446d02d1b800f4c92c210b
Adrielbarr0s <adrielberrios.10@gmail.com>
2025-04-10 18:57:03 -0300

    Correção dos icones dos contatos

--- src/app/component/contato/contato.component.ts
+++ src/app/component/contato/contato.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'; // Importado MatCardModule
 import { MatButtonModule } from '@angular/material/button'; // Importado MatButtonModule
 import { MatIconModule } from '@angular/material/icon'; // Importado MatIconModule
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-contato',
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
 export class ContatoComponent {
   hoveredCard: number | null = null;
-  selectedCard: number | null = null;
   
   toggleCard(index: number): void {
-    this.selectedCard = this.selectedCard === index ? null : index;
+    this.hoveredCard = this.hoveredCard === index ? null : index;
   }
 }
